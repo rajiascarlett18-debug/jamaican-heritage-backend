39bf55e9ee6a2922ed37cb20ea135f958f60f732
rajiascarlett18-debug <rajiascarlett18@gmail.com>
2026-02-25 09:32:14 -0500

    Fix JWT expiresIn production crash

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,9 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
-  });
+const generateToken = (payload) => {
+  return jwt.sign(
+    payload,
+    process.env.JWT_SECRET || 'fallback_secret_dev_only',
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN || '1d'
+    }
+  );
 };
 
 module.exports = generateToken;
